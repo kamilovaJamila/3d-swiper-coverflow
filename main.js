87761195634c7aa384030a98eb78d6b80b9c8d41
kamilovaJamila <jamilakamilova1@gmail.com>
2024-10-01 20:19:14 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.swiper', {
+	effect: 'coverflow',
+	grabCursor: true,
+	centeredSlides: true,
+	slidesPerView: 3,
+	spaceBetween: 30,
+	speed: 600,
+
+	coverflowEffect: {
+		rotate: 50,
+		stretch: 0,
+		depth: 100,
+		modifier: 1,
+		slideShadows: true,
+	},
+
+	loop: true,
+
+	keyboard: {
+		enabled: true,
+	},
+
+	pagination: {
+		el: '.swiper-pagination',
+		dynamicBullets: true,
+		clickable: true,
+	},
+});
